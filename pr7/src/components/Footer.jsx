4fad7ee78b72d4ccfbe7525cc87af99ddb764d0a
DharmGadhiya <gdharm152@gmail.com>
2026-02-14 22:30:02 +0530

    clone_wesite_router_react

---
+++ pr7/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react"; 
+
+// =============================
+// Footer
+// =============================
+function Footer() {
+  return (
+    <footer className="bg-black text-white text-center p-6">
+      <p>© 2026 MyClone. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
